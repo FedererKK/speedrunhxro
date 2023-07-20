48f254ca2ef8e242d1eb3a355feed49ff3d5a508
Federico <wfede21@gmail.com>
2023-07-20 19:04:54 -0400

    create repo

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ const rpc = web3.clusterApiUrl('mainnet-beta');
 const connection = new web3.Connection(rpc, 'confirmed');
 
 const parimutuelWeb3 = new sdk.ParimutuelWeb3(config, connection);
+console.log('ParimutuelWeb3: ', parimutuelWeb3);
